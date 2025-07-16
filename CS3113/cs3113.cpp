4b83e981bf4667f37e5370d6d933ea89d67d5e87
Sebastián <src402@nyu.edu>
2025-07-15 22:29:58 -0400

    Cleaning up

--- CS3113/cs3113.cpp
+++
@@ -1,1 +0,0 @@
-#include "cs3113.h"
--- CS3113/cs3113.h
+++
@@ -1,9 +0,0 @@
-#ifndef CS3113_H
-#define CS3113_H
-#define LOG(argument) std::cout << argument << '\n'
-
-#include "Entity.h"
-
-enum AppStatus { TERMINATED, RUNNING };
-
-#endif // CS3113_H
